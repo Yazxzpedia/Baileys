6b2dcc4c2d50099fed421496b99a343455890677
yazxz <doyokotoy842@gmail.com>
2025-10-04 16:30:55 +0700

    Update engine-requirements.js

--- engine-requirements.js
+++ engine-requirements.js
@@ -2,7 +2,7 @@ const major = parseInt(process.versions.node.split('.')[0], 10);
 
 if (major < 20) {
   console.error(
-  `\n❌ Paket ini memerlukan Node.js versi 20+ agar dapat berjalan dengan baik.\n` +
+  `\n❌ Script ini memerlukan Node.js versi 20+ agar dapat berjalan dengan baik.\n` +
   `   Saat ini Anda menggunakan Node.js versi ${process.versions.node}.\n` +
   `   Silakan upgrade ke Node.js 20+ untuk melanjutkan.\n`
 );
